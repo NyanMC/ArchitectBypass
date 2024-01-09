45a40758827708295404d8fb06614f8f360401fe
ChromaNyan <nyancatz593@gmail.com>
2024-01-09 15:29:16 -0600

    unused imports

--- src/main/java/com/chromanyan/architectbypass/ArchitectBypass.java
+++ src/main/java/com/chromanyan/architectbypass/ArchitectBypass.java
@@ -1,9 +1,7 @@
 package com.chromanyan.architectbypass;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ArchitectBypass.MODID)
